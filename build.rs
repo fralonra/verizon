b6b64cc340a168ba1fdf19a22d6e378c005a7ab9
fralonra <zoronlivingston@gmail.com>
2024-02-29 10:42:38 +0800

    update version label

--- build.rs
+++ build.rs
@@ -7,8 +7,8 @@ const UI_FILE: &'static str = "ui/app.slint";
 
 fn main() {
     // Replace ui contents.
-    let contents = fs::read_to_string(UI_FILE).unwrap();
-    let new_contents = contents.replace("{{version}}", env!("CARGO_PKG_VERSION"));
+    let original_contents = fs::read_to_string(UI_FILE).unwrap();
+    let new_contents = original_contents.replace("{{version}}", env!("CARGO_PKG_VERSION"));
 
     let mut file = OpenOptions::new()
         .write(true)
@@ -20,4 +20,13 @@ fn main() {
 
     // Build ui files.
     slint_build::compile(UI_FILE).unwrap();
+
+    // Restore ui contents.
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(UI_FILE)
+        .unwrap();
+
+    file.write(original_contents.as_bytes()).unwrap();
 }
